fcdea2c0978e13293ed2bcf83afb3051b40a29c4
Charlesjahn <72029865+Charlesjahn@users.noreply.github.com>
2020-11-17 12:49:09 +0000

    Update CA_2_T&T.java
    lets start coding

--- CA_2_T&T.java
+++ CA_2_T&T.java
@@ -1,3 +1,19 @@
-public class CA_2_T&T {
-    
-}
+package homework3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Charles Franklin Jahn
+ * @author 
+ * @author 
+ * @author 
+ * @author 
+ */
+public class Homework3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner myKB = new Scanner(System.in);
